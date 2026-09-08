b4650862d6cbc2a24d53926d62a5cfe6ab698312
JakeThePukaPieConsumer <jdjm@2009@outlook.com>
2026-09-08 16:52:17 +0100

    add: round result enum

--- include/game.h
+++ include/game.h
@@ -7,4 +7,13 @@ typedef enum {
     STATE_ROUND_OVER,
 } GameState;
 
+typedef enum {
+    RESULT_NONE,
+    RESULT_PLAYER_WIN,
+    RESULT_DEALER_WIN,
+    RESULT_PUSH,
+    RESULT_PLAYER_BUST,
+    RESULT_DEALER_BUST
+} RoundResult;
+
 void game_run(void);
