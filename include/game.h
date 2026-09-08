335f56af9db69d4b05a52b080b40682ee9802a74
JakeThePukaPieConsumer <jdjm@2009@outlook.com>
2026-09-08 17:35:13 +0100

    add: win and loss condition enums

--- include/game.h
+++ include/game.h
@@ -6,4 +6,21 @@ typedef enum {
     RESULT_PUSH,
 } RoundResult;
 
+typedef enum {
+    REASON_NORMAL,              // Nothing special
+    REASON_BLACKJACK,           // Natural
+    REASON_TWENTY_ONE,          // 3+ Cards
+    REASON_SUITED_BLACKJACK,    // Natural with same suit
+    REASON_LUCKY_DOUBLE_DOWNER, // Double down but still won
+    REASON_SPLIT_WIN,           // Split but still won
+    REASON_FIVE_CARD_CHARLIE,   // 5 cards without a bust
+} WinCondition;
+
+typedef enum {
+    LOSS_NORMAL,      // Nothing speical
+    LOSS_BUST,        // Went over 21
+    LOSS_SPLIT,       // Lost a split hand
+    LOSS_DOUBLE_DOWN, // Lost a double down
+} LossCondition;
+
 void game_run(void);
