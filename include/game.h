72c05235a4a5adf991525883bc169deebfefc64e
JakeThePukaPieConsumer <jdjm@2009@outlook.com>
2026-09-08 17:34:35 +0100

    refactor: round result enum

--- include/game.h
+++ include/game.h
@@ -1,19 +1,9 @@
 #pragma once
 
 typedef enum {
-    STATE_START,
-    STATE_PLAYER_TURN,
-    STATE_DEALER_TURN,
-    STATE_ROUND_OVER,
-} GameState;
-
-typedef enum {
-    RESULT_NONE,
     RESULT_PLAYER_WIN,
     RESULT_DEALER_WIN,
     RESULT_PUSH,
-    RESULT_PLAYER_BUST,
-    RESULT_DEALER_BUST
 } RoundResult;
 
 void game_run(void);
