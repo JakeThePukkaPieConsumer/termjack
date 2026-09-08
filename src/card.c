cf624a798b3651773a346677d3369ebb5a72b6ac
JakeThePukaPieConsumer <jdjm@2009@outlook.com>
2026-09-08 18:49:47 +0100

    fix: rename x, y to row, col in card draw func

--- src/card.c
+++ src/card.c
@@ -19,7 +19,7 @@ int card_value(const Card *card) {
     return card->rank;
 }
 
-void card_draw(const Card *card, int x, int y) {
+void card_draw(const Card *card, int row, int col) {
     int color_pair = (card->suit == HEARTS || card->suit == DIAMONDS) ? 2 : 1;
 
     attron(COLOR_PAIR(color_pair));
